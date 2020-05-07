f5942b5bbcd8439cb9e2db8d92721cbe447598fd
Arthur Pichlkostner <apichlkostner@gmx.de>
2020-05-07 23:54:27 +0200

    Using libpng++

--- include/color.h
+++ include/color.h
@@ -2,13 +2,33 @@
 #define COLOR_H
 
 #include <iostream>
+#include <memory>
+#include <png++/png.hpp>
 
 #include "vec3.h"
 
-void write_color(std::ostream &out, color pixel_color) {
-  // Write the translated [0,255] value of each color component.
-  out << static_cast<int>(255.999 * pixel_color.x()) << ' ' << static_cast<int>(255.999 * pixel_color.y()) << ' '
-      << static_cast<int>(255.999 * pixel_color.z()) << '\n';
-}
+class ImageWrapper {
+ public:
+  ImageWrapper(std::string filename, uint32_t width, uint32_t height)
+      : image_(std::make_unique<png::image<png::rgb_pixel>>(width, height)),
+        filename_(filename),
+        width_(width),
+        height_(height) {}
+
+  void write_color(uint32_t x, uint32_t y, color pixel_color) {
+    // Write the translated [0,255] value of each color component.
+    (*image_)[height_ - 1 - y][x] =
+        png::rgb_pixel(static_cast<int>(255.999 * pixel_color.x()), static_cast<int>(255.999 * pixel_color.y()),
+                       static_cast<int>(255.999 * pixel_color.z()));
+  }
+
+  void write() { image_->write(filename_); }
+
+ private:
+  std::unique_ptr<png::image<png::rgb_pixel>> image_;
+  uint32_t width_;
+  uint32_t height_;
+  std::string filename_;
+};
 
 #endif
--- sources/main.cpp
+++ sources/main.cpp
@@ -3,32 +3,54 @@
 #include "color.h"
 #include "ray.h"
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b * b - 4 * a * c;
+  if (discriminant < 0) {
+    return -1.0;
+  } else {
+    return (-b - sqrt(discriminant)) / (2.0 * a);
+  }
+}
+
 color ray_color(const ray& r) {
+  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+  if (t > 0.0) {
+    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+    return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+  }
   vec3 unit_direction = unit_vector(r.direction());
-  auto t = 0.5 * (unit_direction.y() + 1.0);
+  t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main() {
   const auto aspect_ratio = 16.0 / 9.0;
-  const int image_width = 384;
+  const int image_width = 1024;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
 
-  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+  ImageWrapper image("raytrace.png", image_width, image_height);
+
   point3 origin(0.0, 0.0, 0.0);
   vec3 horizontal(4.0, 0.0, 0.0);
   vec3 vertical(0.0, 2.25, 0.0);
   point3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, 1);
+
   for (int j = image_height - 1; j >= 0; --j) {
-    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+    std::cout << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
       auto u = double(i) / (image_width - 1);
       auto v = double(j) / (image_height - 1);
       ray r(origin, lower_left_corner + u * horizontal + v * vertical);
       color pixel_color = ray_color(r);
-      write_color(std::cout, pixel_color);
+      image.write_color(i, j, pixel_color);
     }
   }
 
-  std::cerr << "\nDone.\n";
+  image.write();
+
+  std::cout << "\nDone.\n";
 }

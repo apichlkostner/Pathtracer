5ae12d67c1f8c299283452eb7c2d3f525badebfc
Arthur Pichlkostner <apichlkostner@gmx.de>
2020-05-10 19:33:45 +0200

    Performance optimization: AMD math library Faster random number generator

--- include/rtweekend.h
+++ include/rtweekend.h
@@ -3,9 +3,9 @@
 
 #include <cmath>
 #include <cstdlib>
+#include <functional>
 #include <limits>
 #include <memory>
-#include <functional>
 #include <random>
 
 // Constants
@@ -18,11 +18,14 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees) { return degrees * pi / 180; }
 
 inline double random_double() {
-    // thread_local to speed up, else the threads wait most of the time
-    static thread_local std::mt19937 generator;
-    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  // thread_local to speed up, else the threads wait most of the time
+  //static thread_local std::mt19937 generator;  // slow
+  //static thread_local std::ranlux48_base generator; // faster
+  // fast random number generator
+  static thread_local std::minstd_rand generator; // fastest
+  std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
-    return distribution(generator);
+  return distribution(generator);
 }
 
 inline double random_double(double min, double max) {
